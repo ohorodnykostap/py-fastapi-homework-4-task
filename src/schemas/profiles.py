3bf755d23f515c5097b64821f8f26849c4f5f368
Ostap Ohorodnyk <ostap.ohorodnyk@gmail.com>
2026-02-03 18:21:06 +0200

    Fixed code style

--- src/schemas/profiles.py
+++ src/schemas/profiles.py
@@ -11,7 +11,6 @@ from validation import (
 )
 
 
-
 class ProfileCreateSchema(BaseModel):
     first_name: str
     last_name: str
@@ -150,4 +149,3 @@ class ProfileResponseSchema(BaseModel):
     date_of_birth: date
     info: str
     avatar: HttpUrl
-
